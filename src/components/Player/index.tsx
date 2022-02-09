a17530d9859793eaed84b0d09cf3ecd46f62c99e
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-02-09 19:15:28 -0300

    configuração inicial do projeto

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,56 @@
+
+import styles from './styles.module.scss';
+
+export function Player() {
+
+
+    return (
+        <div className={styles.playerContainer}>
+            <header>
+                <img src="a" alt="Tocando agora" />
+                <strong> Tocando agora</strong>
+            </header>
+
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer className={styles.empty}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider}></div>
+                    </div>
+
+                    <span>00:00</span>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button type="button">
+                        <img src="" alt="Embaralhar" />
+                    </button>
+
+                    <button type="button">
+                        <img src="" alt="Tocar anterior" />
+                    </button>
+
+                    <button type="button" className={styles.playButton}>
+                        <img src="" alt="tocar" />
+                    </button>
+
+                    <button type="button">
+                        <img src="" alt="Tocar próxima" />
+                    </button>
+
+                    <button type="button">
+                        <img src="" alt="Repetir" />
+                    </button>
+
+
+
+                </div>
+            </footer>
+        </div>
+
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import {Header} from '../components/Header';
+export default function Home() {
+  return (
+<h1>index </h1>
+  )
+}
