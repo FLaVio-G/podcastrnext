790d53e2a0ac3e8d089477648120d9a23ccadc4e
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-02-11 16:57:39 -0300

    configurando Json Server SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
-import {Header} from '../components/Header';
-export default function Home() {
+// SPA
+//SSR
+//SSG
+
+import { useEffect } from "react"
+
+export default function Home(props) {
+  console.log(props.episodes)
+ 
   return (
-<h1>index </h1>
+<h1>index</h1>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  
+  return { 
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
