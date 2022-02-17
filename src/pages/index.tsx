54151914dcde0193672b41c939fccaac6360b75b
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-02-17 16:59:36 -0300

    Implementação do Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@
 
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
@@ -53,8 +54,11 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                 />
 
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                  <a >{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
+                 
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
 
