53e651f45ed64737e118b90fe4979290be7ff81f
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-02-22 23:04:03 -0300

    Criação contexts e dentro PlayerContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+
+
+
 import styles from '../styles/app.module.scss';
+
+import { PlayerContext } from '../contexts/playerContext';
+
 
 function MyApp({ Component, pageProps }) {
   return (
+    <PlayerContext.Provider value={'Diego'}>
     <div className={styles.wrapper}>
       <main>
         <Header />
@@ -13,6 +20,8 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player />
     </div>
+      </PlayerContext.Provider>
+    
   )
 }
 
